757d22d6fe8688eb467f3e7e39e6460e3a39cd55
Русанов Олег Сергеевич <o.rusanov@univ.priv>
2021-09-16 10:16:38 +0300

    add \n

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args){
-    System.out.print("\n Hello World!");
+    System.out.print("\n Hello World \n");
 System.out.print("\n New Feature!");
     }
 }
